57ed4146ae32970714ce71ea7c63286bb10f151d
rufeex <fortronin364@gmail.com>
2026-03-23 11:50:20 +0300

    1-3

--- solutions/3-isset.js
+++ solutions/3-isset.js
@@ -1,3 +1,11 @@
 // BEGIN
+const get = (city, index, znach = null) => {
+  // Проверяем, существует ли элемент по указанному индексу
+  if (index >= 0 && index < city.length) {
+    return city[index];
+  }
+  return znach;
+};
 
+export { get };
 // END
