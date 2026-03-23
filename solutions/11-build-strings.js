ef74584dfa6c8c55d1ce368de1f9ce0937ba0493
rufeex <fortronin364@gmail.com>
2026-03-23 14:26:50 +0300

    1-12

--- solutions/11-build-strings.js
+++ solutions/11-build-strings.js
@@ -1,3 +1,20 @@
 // BEGIN
+const buildDefinitionList = (definitions) => {
+    if (definitions.length === 0) {
+        return '';
+    }
+    
+    let result = '<dl>';
+    
+    for (let i = 0; i < definitions.length; i++) {
+        const term = definitions[i][0];
+        const description = definitions[i][1];
+        result += `<dt>${term}</dt><dd>${description}</dd>`;
+    }
+    
+    result += '</dl>';
+    return result;
+};
 
+export default buildDefinitionList;
 // END
