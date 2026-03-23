be438da632c26120b3e9f3b73bd04fbda5ad163e
rufeex <fortronin364@gmail.com>
2026-03-23 12:56:30 +0300

    1-7

--- solutions/6-aggregation.js
+++ solutions/6-aggregation.js
@@ -1,3 +1,16 @@
 // BEGIN
+const calculateSum = (massiv) => {
+    if (massiv.length != 0){
+        let otwet = 0;
+        for (let i = 0; i < massiv.length; i++){
+            if (massiv[i] % 3 === 0){
+                otwet += massiv[i];
+            }
+        }
+        return otwet;
+    }
+    return 0;
+}
 
+export { calculateSum };
 // END
--- solutions/7-for-of.js
+++ solutions/7-for-of.js
@@ -1,3 +1,14 @@
 // BEGIN
+const calculateAverage = (massiv) => {
+    if (massiv.length != 0){
+        let otwet = 0;
+        for (let i = 0; i < massiv.length; i++){
+            otwet += massiv[i];
+        }
+        return otwet / massiv.length;
+    }
+    return null;
+}
 
+export default calculateAverage;
 // END
