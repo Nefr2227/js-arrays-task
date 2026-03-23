de7cebfb9880bb90d43b5dda832e86644b451dc8
rufeex <fortronin364@gmail.com>
2026-03-23 12:21:31 +0300

    1-5

--- solutions/5-references.js
+++ solutions/5-references.js
@@ -1,3 +1,12 @@
 // BEGIN
+const reverse = (massiv) => {
+    const length = massiv.length;
+    for (let i = 0; i < Math.floor(length / 2); i++) {
+        const temp = massiv[i];
+        massiv[i] = massiv[length - 1 - i];
+        massiv[length - 1 - i] = temp;
+    }
+}
 
+export { reverse };
 // END
